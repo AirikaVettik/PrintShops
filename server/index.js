a1837d46183539805907a3c83ecebc29084de16a
airikavettik <airikavettik@Airikas-MBP.lan>
2021-06-01 11:21:02 +0300

    working server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express ();
-app.use(cors())
 
-app.listen (3000, () => {
-    console.log("Server started on 3000")
-})
+app.use(bodyParser.json)
+app.use(cors());
+
+const port = process.env.PORT || 3000
+
+app.listen (port, () => {
+    console.log(`Server started on port ${port}`)
+});
