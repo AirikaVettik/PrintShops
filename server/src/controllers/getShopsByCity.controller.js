715d022bb063d0928bb19247c63f7fd06d826beb
Airika Vettik <airikavettik@Airikas-iMac.lan>
2021-06-03 11:51:10 +0300

    getShopsByCity

--- server/src/controllers/getShopsByCity.controller.js
+++ server/src/controllers/getShopsByCity.controller.js
@@ -5,11 +5,10 @@ const Shops = db.Shops
 module.exports = async function (req, res) { 
   try {
 
-    const getShopsByLocation = await Shops.find ({ 
+    const getShopsByCity = await Shops.find ({ 
       "location.city" : req.params.city
     })
-    res.status(200).json({ getShopsByLocation })
-
+    res.status(200).json({ getShopsByCity})
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: error.message })
