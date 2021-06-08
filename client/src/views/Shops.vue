25472da193ef9634689b7566ce4fc5ece90447bf
Airika Vettik <airikavettik@Airikas-iMac.lan>
2021-06-08 12:14:30 +0300

    deleteShop

--- client/src/views/Shops.vue
+++ client/src/views/Shops.vue
@@ -29,27 +29,27 @@
 
                     <b-list-group-item>
                     <br>
-                    <h4>Tooted/teenused: </h4> <br>
-                    <div v-for="items in shop.products">
-                        <li>{{ items.size}} {{ items.paper}} {{ items.price}}€ ({{ items.name}})<br>
-                        </li>
-                    </div>
-                    <br>
-                    </b-list-group-item>
+                            <h4>Tooted/teenused: </h4> <br>
+                            <div v-for="items in shop.products">
+                                <li>{{ items.size}} {{ items.paper}} {{ items.price}}€ ({{ items.name}})<br>
+                                </li>
+                            </div>
+                            <br>
+                            </b-list-group-item>
 
-                    </b-list-group>
-                    <b-card-body>
+                            </b-list-group>
+                            <b-card-body>
 
-                    <b-button variant="outline-info" class="mb-2">
-                    <b-icon icon="wrench" font-scale="2" aria-hidden="true">
-                    <router-link v-bind:to="'/editshop/'"></router-link>
-                    </b-icon>
-                    </b-button>
-                    <b-button variant="outline-info" class="mb-2">
-                    <b-icon icon="trash-fill" font-scale="2" aria-hidden="true"
-                    @click="deleteProduct(data.item._id, data.index)"></b-icon>
-                    </b-button>
-                    </b-card-body>
+                            <b-button variant="outline-info" class="mb-2">
+                            <b-icon icon="wrench" font-scale="2" aria-hidden="true">
+                            <router-link v-bind:to="'editshop/'"></router-link>
+                            </b-icon>
+                            </b-button>
+                            <b-button variant="outline-info" class="mb-2">
+                            <b-icon icon="trash-fill" font-scale="2" aria-hidden="true"
+                            @click="deleteProduct(shop._id, index)"></b-icon>
+                            </b-button>
+                            </b-card-body>
                     
             <b-card-footer>
             Lisatud: {{ moment(shop.createdDate).format("DD.MM.YYYY") }} 
@@ -73,21 +73,32 @@ export default {
     },
 
     async created () {
-
-    const shops = await axios({
-      url: 'api/shops',
-      method: 'GET'
-    })
-    console.log("shops", shops.data)
-    this.shops = shops.data    
-  },
-  methods: {
-    showProducts(items){
-      console.log('products', products)
-      this.productItems = products
-      console.log(items)
+        await this.getMyShops()
     },
-}}
+    
+    methods: {
+
+        async getMyShops() {
+            const shops = await axios({
+            url: 'api/shops',
+            method: 'GET'
+            })
+            console.log("shops", shops.data)
+            this.shops = shops.data    
+            
+        },
+
+        async deleteProduct(_id, index) {
+            if (confirm ("Kas oled kindel, et soovid toodet kustutada?")) {
+                console.log(_id)
+                await axios ({
+                url:  `api/shop/${_id}` ,
+                method: 'DELETE'
+                })
+                await this.getMyShops()
+                }
+            },
+    }}
 </script>
 
 <style>
